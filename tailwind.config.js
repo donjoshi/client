425301cc74c524da4c3f82c7c2ebd71cb944f3d1
Don Joshi N <97145305+donjoshi@users.noreply.github.com>
2024-07-24 11:18:36 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary:"#F5385D",
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F5385D',
+      },
+    },
+  },
+  plugins: [],
+}
