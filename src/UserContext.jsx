a676b50cf5982f6623070d6c417ac923c57db28f
Don Joshi N <97145305+donjoshi@users.noreply.github.com>
2024-07-26 12:27:50 +0530

    logout issue solved

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -8,6 +8,7 @@ export const UserContext = createContext({});
 export function UserContextProvider({ children }) {
 
     const [user, setUser] = useState(null);
+    const [ready, setReady] = useState(false);
 
     useEffect( () => {
         if(!user)
@@ -15,12 +16,13 @@ export function UserContextProvider({ children }) {
             const {data}= axios.get('/profile').then(({data})=>
             {
                 setUser(data);
+                setReady(true);
             });
         }
     }, []);
 
     return (
-        <UserContext.Provider value={{ user,setUser}}>
+        <UserContext.Provider value={{ user,setUser,ready}}>
             {children}
         </UserContext.Provider>
         
