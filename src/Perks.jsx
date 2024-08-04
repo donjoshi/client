baebf7878cae800609bce95bc47d4b10991271c8
Don Joshi N <97145305+donjoshi@users.noreply.github.com>
2024-08-04 17:04:55 +0530

    committed

--- src/Perks.jsx
+++ src/Perks.jsx
@@ -4,7 +4,8 @@ export default function Perks({ selected, onChange }) {
 
 
     function handleCbClick(name) {
-        alert(name);        
+        alert(name); 
+               
     }
 
     return (
